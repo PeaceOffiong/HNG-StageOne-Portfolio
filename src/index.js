1f7d79d62b38c5508b8fc3fe39134a8a064d689b
PeaceOffiong <97194474+PeaceOffiong@users.noreply.github.com>
2023-09-10 23:48:41 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import "./styles.css";
+document.addEventListener("DOMContentLoaded", function () {
+    const timeStamp = document.getElementById("date")
+    const date = document.getElementById("time");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+    const getTime = () => {
+        const currentTime = new Date();
+
+        const splits = {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true,
+        };
+        timeStamp.innerHTML = currentTime.toLocaleTimeString(undefined, splits);
+        date.innerHTML = currentTime.toDateString();
+    }
+
+    getTime();
+
+    setInterval(getTime, 1000);
+});
